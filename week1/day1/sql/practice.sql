22c6be0011ec36ab49b21bc1de5d207963628cb4
Naveen-Reddy1045 <vdnaveenreddy@gmail.com>
2026-05-06 17:48:22 +0530

    Add SQL queries for Employee and Department data retrieval
    Added SQL queries to select data from Employee and Department tables.

--- week1/day1/sql/practice.sql
+++ week1/day1/sql/practice.sql
@@ -1,1 +1,13 @@
-
+-- Select all columns from the Employee table.
+    select *from Empolyee;
+-- Select only the name and salary columns from the Employee table.
+      select name, salary from Empolyee;
+-- Select employees who are older than 30.
+      select *from Empolyee
+      where age > 30;
+-- Select the names of all departments.
+      select name from Employee;
+-- Select employees who work in the IT department.
+      SELECT e.name FROM Employee e
+      JOIN Department d
+      ON e.department_id = d.department_id;
