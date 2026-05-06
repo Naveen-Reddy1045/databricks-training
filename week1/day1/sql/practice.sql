30aa54a6d083ad4bc940c808bacc427e771c91cf
Naveen-Reddy1045 <vdnaveenreddy@gmail.com>
2026-05-06 17:59:43 +0530

    Basic and String matching SQL practice queries
    Corrected formatting and spacing issues in SQL queries.

--- week1/day1/sql/practice.sql
+++ week1/day1/sql/practice.sql
@@ -1,13 +1,37 @@
 -- Select all columns from the Employee table.
     select *from Empolyee;
+
 -- Select only the name and salary columns from the Employee table.
-      select name, salary from Empolyee;
+    select name, salary from Empolyee;
+
 -- Select employees who are older than 30.
-      select *from Empolyee
-      where age > 30;
+    select *from Empolyee
+    where age > 30;
+
 -- Select the names of all departments.
-      select name from Employee;
+    select name from Employee;
+
 -- Select employees who work in the IT department.
-      SELECT e.name FROM Employee e
-      JOIN Department d
-      ON e.department_id = d.department_id;
+    SELECT e.name FROM Employee e
+    JOIN Department d
+    ON e.department_id = d.department_id;
+
+-- Select employees whose names start with 'J'.
+    select *from Employee
+    where name like "J%";
+
+--Select employees whose names end with 'e'.
+    select *from Employee
+    where name like "%e";
+
+--Select employees whose names contain 'a'.
+    select *from Employee
+    where name like "%a%";
+
+--Select employees whose names are exactly 9 characters long.
+    select *from Employee
+    where length(name) = 9;
+
+--Select employees whose names have 'o' as the second character.
+    select *from Employee
+    where name = '_o%';
